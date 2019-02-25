1d69a0948fa22d7fed6f2d1d9d4fd0ad20a52f83
Arun Sharma <arunsharma9125@gmail.com>
2019-02-26 00:09:18 +0530

    Missing number 0toN

---
+++ leetCode/Test268.java
@@ -0,0 +1,33 @@
+package leetCode;
+
+import java.util.Arrays;
+
+public class Test268 {
+	public int missingNumber(int[] nums) {
+		if (nums.length == 1 && nums[0] == 0) {
+			return 1;
+
+		} else if (nums.length == 1 && nums[0] == 1) {
+			return 0;
+
+		}
+		Arrays.sort(nums);
+
+		int first = nums[0];
+		if (first != 0) {
+			return 0;
+		}
+
+		for (int i = 0; i <= nums.length; i++) {
+			if (i + 1 == nums.length) {
+				return i + 1;
+			} else if ((i + 1) == nums[i + 1]) {
+				continue;
+			} else {
+				return i + 1;
+			}
+
+		}
+		return 0;
+	}
+}
